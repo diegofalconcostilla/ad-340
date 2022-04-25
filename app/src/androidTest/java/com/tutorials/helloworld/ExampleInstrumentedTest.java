994c9fa6a925af2a83879b9e108115397b859b00
diegofalconc <diegofalconcostilla94@gmail.com>
2022-04-24 22:02:01 -0700

    circle ci configurations

--- app/src/androidTest/java/com/tutorials/helloworld/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tutorials/helloworld/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("eu.tutorials.helloworld", appContext.getPackageName());
+        assertEquals("com.tutorials.helloworld", appContext.getPackageName());
     }
 }
