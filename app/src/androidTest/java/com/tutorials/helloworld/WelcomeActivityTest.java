555881b9ae19b7a31b84da6fb7a671ce7af77eeb
diegofalconc <diegofalconcostilla94@gmail.com>
2022-05-05 11:50:14 -0700

    Assignment 5 tests

--- app/src/androidTest/java/com/tutorials/helloworld/WelcomeActivityTest.java
+++ app/src/androidTest/java/com/tutorials/helloworld/WelcomeActivityTest.java
@@ -3,9 +3,11 @@ package com.tutorials.helloworld;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.tutorials.helloworld.RecyclerViewMatcher.withRecyclerView;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -31,5 +33,8 @@ public class WelcomeActivityTest {
     public void succesfulNavigationToMatches(){
         onView(withContentDescription("Open navigation drawer")).perform(click());
         onView(withId(R.id.matches_menu_item)).perform(click());
+
+        onView(withRecyclerView(R.id.recycler_view).atPosition(0))
+                .check(matches(hasDescendant(withText("Test 1"))));
     }
 }
